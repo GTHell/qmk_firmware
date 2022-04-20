8a440b270b8628ed510616b1889b7ef70d1ceaca
Sith <sosxradar@gmail.com>
2022-04-21 02:56:30 +0700

    update rgb matrix for corne, update redox keymap

---
+++ keyboards/crkbd/keymaps/gthell/rules.mk
@@ -0,0 +1,7 @@
+MOUSEKEY_ENABLE = no     # Mouse keys
+RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
+RGB_MATRIX_ENABLE = yes # Allow RGB underlight to be controlled by a separate script.
+VIA_ENABLE      = yes    # Enable VIA
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
