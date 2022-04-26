c8c68261f9327f3a21aa40fb8be00404288ede04
Sith <sosxradar@gmail.com>
2022-04-27 03:02:54 +0700

    update rgb

--- keyboards/crkbd/keymaps/gthell/config.h
+++ keyboards/crkbd/keymaps/gthell/config.h
@@ -53,10 +53,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #   define RGB_MATRIX_KEYPRESSES // reacts to keypresses
 // #   define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
 // #   define RGB_DISABLE_AFTER_TIMEOUT 0 // number of ticks to wait until disabling effects
-// #   define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
-// #   define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#   define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
+#   define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 // #   define RGB_MATRIX_LED_PROCESS_LIMIT (DRIVER_LED_TOTAL + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
-// #   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
+#   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
 #    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
 #    define RGB_MATRIX_HUE_STEP 8
 #    define RGB_MATRIX_SAT_STEP 8
@@ -81,9 +81,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #    define ENABLE_RGB_MATRIX_CYCLE_ALL
 
 // #    define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
-#    define ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
+// #    define ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
 // #    define ENABLE_RGB_MATRIX_CYCLE_OUT_IN
-#    define ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+// #    define ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
 // #    define ENABLE_RGB_MATRIX_DUAL_BEACON
 
 // #    define ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
--- keyboards/crkbd/keymaps/gthell/rules.mk
+++ keyboards/crkbd/keymaps/gthell/rules.mk
@@ -1,6 +1,6 @@
 MOUSEKEY_ENABLE = no     # Mouse keys
 RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
-RGB_MATRIX_ENABLE = no # Allow RGB underlight to be controlled by a separate script.
+RGB_MATRIX_ENABLE = yes # Allow RGB underlight to be controlled by a separate script.
 VIA_ENABLE      = yes    # Enable VIA
 OLED_ENABLE     = yes
 OLED_DRIVER     = SSD1306
